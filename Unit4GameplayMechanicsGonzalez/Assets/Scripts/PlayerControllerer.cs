04e5c70e772c0e3551eb49271324852d4bcef65d
GonzalezKe <710293@student.dusd.net>
2023-09-13 13:47:22 -0700

    on part 3

--- Unit4GameplayMechanicsGonzalez/Assets/Scripts/PlayerControllerer.cs
+++ Unit4GameplayMechanicsGonzalez/Assets/Scripts/PlayerControllerer.cs
@@ -10,6 +10,11 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     public bool hasPowerup = false;
     public GameObject powerupIndicator;
+    public PowerUpType currentPowerUp  = PowerUpType.None;
+
+    public GameObject rocketPrefab;
+    private GameObject tmpRocket;
+    private Coroutine powerupCountdown;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +31,11 @@ public class PlayerController : MonoBehaviour
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
 
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.52f, 0);
+
+        if (currentPowerUp == PowerUpType.Rockets && Input.GetKeyDown(KeyCode.F)) 
+        { 
+            LaunchRockets(); 
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -33,9 +43,16 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("Powerup"))
         {
             hasPowerup = true;
+            currentPowerUp = other.gameObject.GetComponent<PowerUp>().powerUpType;
             powerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
             StartCoroutine(PowerupCountdownRoutine());
+
+            if (powerupCountdown != null) 
+            { 
+                StopCoroutine(powerupCountdown); 
+            }
+            powerupCountdown = StartCoroutine(PowerupCountdownRoutine());
         }
     }
 
@@ -43,18 +60,29 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(7);
         hasPowerup = false;
+        currentPowerUp = PowerUpType.None;
         powerupIndicator.gameObject.SetActive(false);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        if (collision.gameObject.CompareTag("Enemy") && currentPowerUp == PowerUpType.Pushback)
         {
             Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
             Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
 
             Debug.Log("Collide with " + collision.gameObject.name + " with powerup set to " + hasPowerup);
             enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            Debug.Log("Player collided with: " + collision.gameObject.name + " with powerup set to " + currentPowerUp.ToString());
+        }
+    }
+
+    void LaunchRockets()
+    {
+        foreach(var enemy in FindObjectsOfType<Enemy>())
+        {
+            tmpRocket = Instantiate(rocketPrefab, transform.position + Vector3.up, Quaternion.identity);
+            tmpRocket.GetComponent<RocketBehaviour>().Fire(enemy.transform);
         }
     }
 }
--- Unit4GameplayMechanicsGonzalez/Assets/Scripts/RocketBehaviour.cs
+++ Unit4GameplayMechanicsGonzalez/Assets/Scripts/RocketBehaviour.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class RocketBehaviour : MonoBehaviour
 {
+
+    private Transform target;
+    private float speed = 15.0f;
+    private bool homing;
+
+    private float rocektStrength = 15.0f;
+    private float aliveTimer = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,32 @@ public class RocketBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(homing && target != null)
+        {
+            Vector3 moveDirection = (target.transform.position - transform.position).normalized;
+            transform.position = moveDirection * speed * Time.deltaTime;
+            transform.LookAt(target);
+        }
+    }
+
+    public void Fire(Transform newTarget)
+    {
+        target = homingTarget;
+        homing = true;
+        Destroy(gameObject, aliveTimer);
+    }
+
+    void OnCollisionEnter(Collision col)
+    { 
+        if (target != null)
+        {
+            if (col.gameObject.CompareTag(target.tag))
+            {
+                Rigidbody targetRigidbody = col.gameObject.GetComponent<Rigidbody>();
+                Vector3 away = -col.contacts[0].normal;
+                targetRigidbody.AddForce(away * rocketStrength, ForceMode.Impulse);
+                Destroy(gameObject);
+            }
+        }
     }
 }
